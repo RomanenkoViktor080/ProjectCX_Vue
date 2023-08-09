bda756c2f74ce97bb8e215b32a2038ba2e28fe58
RomanenkoViktor080 <romanenkoviktor080@gmail.com>
2023-08-09 09:06:13 +0300

    Установка vue, vue-router, vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import '../../resources/sass/app.scss'
+import {createApp} from 'vue';
+import AppVueComponent from './components/app.vue';
+import router from "./router.js";
+
+const appVue = createApp(AppVueComponent);
+appVue.mount('#app');
+appVue.use(router);
